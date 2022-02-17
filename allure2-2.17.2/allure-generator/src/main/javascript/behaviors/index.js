e1189f28c0f251f9c69fe32f7da359a5621b924c
Diego Linares <diegoalonsoll@hotmail.com>
2022-02-17 10:19:45 -0500

    allure report folder added

--- allure2-2.17.2/allure-generator/src/main/javascript/behaviors/index.js
+++
@@ -1,5 +0,0 @@
-import ClipboardBehavior from "./ClipboardBehavior";
-import LoadBehavior from "./LoadBehavior";
-import TooltipBehavior from "./TooltipBehavior";
-
-export { LoadBehavior, TooltipBehavior, ClipboardBehavior };
